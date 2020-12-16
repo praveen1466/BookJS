3cce1fc9b3086ce6233f5c2000a854e79a514eda
praveen1466 <praveensknl7@gmail.com>
2020-12-16 16:35:29 +0530

    Added: sorting entries by city, state, pincode

--- AddressBook.js
+++ AddressBook.js
@@ -154,3 +154,23 @@ for(let book in addressBookArray){
     addressBookArray.sort(book.firstname);
 }
 addressBookArray.forEach((count)=>console.log(count.toString()));
+
+
+//UC12 Sorting entries by city, state, pincode
+console.log("\nSorting by City ")
+for(let book in addressBookArray){
+    addressBookArray.sort(book.city)
+}
+addressBookArray.forEach((count)=>console.log(count.toString()))
+
+console.log("Sorting by State\n")
+for(let book in addressBookArray){
+    addressBookArray.sort(book.state)
+}
+addressBookArray.forEach((count)=>console.log(count.toString()))
+
+console.log("Sorting by Pincode\n")
+for(let book in addressBookArray){
+    addressBookArray.sort(book.pincode)
+}
+addressBookArray.forEach((count)=>console.log(count.toString()))
